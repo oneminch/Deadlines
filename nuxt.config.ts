f3d9dbdb25db37dc22ee85839b8d152dc909e34d
oneminch <27751688+oneminch@users.noreply.github.com>
2024-08-15 20:30:03 -0400

    init: Nuxt 3 project setup using CLI

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+import Aura from '@primevue/themes/aura';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: [
+    '@primevue/nuxt-module'
+  ],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura
+      }
+    }
+  }
+});

3f3b89218dac60b36bcaef0809c19e1da8e55404
oneminch <27751688+oneminch@users.noreply.github.com>
2024-08-23 20:46:00 -0400

    feat: create custom types

---
+++ utils/types.ts
@@ -0,0 +1,23 @@
+import type { NuxtApp } from "#app";
+
+interface CustomNuxtApp extends NuxtApp {
+  $offlineDB: LocalForage;
+}
+
+interface DeadlineItem {
+  id: string;
+  task: string;
+  date: string;
+  overdue: boolean;
+  color: string;
+}
+
+interface Options {
+  firstTime: boolean;
+  toastEnabled: boolean;
+  darkThemeEnabled: boolean;
+}
+
+type DBResponseType = DeadlineItem[] | Options | null;
+
+export type { DeadlineItem, Options, DBResponseType, CustomNuxtApp };

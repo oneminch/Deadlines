6883ba4939cd120347f008d94be0bf54ae79fb54
oneminch <27751688+oneminch@users.noreply.github.com>
2024-10-17 02:37:26 -0400

    feat: create modal components based on useModal composable

---
+++ components/modals/AppInputToggle.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  import AppInputModal from "./AppInputModal.vue";
+
+  const { openModal } = useModal();
+
+  const openInputModal = () => {
+    openModal(AppInputModal);
+  };
+</script>
+
+<template>
+  <button
+    aria-label="Toggle Input"
+    title="Toggle Input"
+    class="icon-button"
+    @click="openInputModal">
+    <Icon name="ph:plus-bold" />
+  </button>
+</template>

c6c81d2d45127f97d345fc71d35f79b6ceabb529
oneminch <27751688+oneminch@users.noreply.github.com>
2025-09-25 12:32:01 -0400

    fix: add higher z-index to mobile nav bar

--- app/components/layout/app-nav-bar.vue
+++ app/components/layout/app-nav-bar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex items-center gap-x-2 fixed bottom-4 left-1/2 overflow-hidden transform -translate-x-1/2">
+    class="flex items-center gap-x-2 fixed bottom-4 left-1/2 overflow-hidden transform -translate-x-1/2 z-30">
     <div
       class="flex sm:hidden items-center gap-x-2 shadow-md rounded-full p-2 sm:p-1 bg-violet-50 dark:bg-zinc-900 border-2 sm:border border-violet-500">
       <app-input-modal trigger-class="rounded-full! sm:hidden" />
